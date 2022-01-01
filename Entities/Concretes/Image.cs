2c64095f82ee8f9f8bb0f43a8c3f5e71e62970a1
Mustafa Paydaş <mustafapaydas@hacettepe.edu.tr>
2022-01-01 16:15:11 +0300

    revise2

---
+++ Entities/Concretes/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities.Abstracts;
+
+namespace Entities.Concretes
+{
+    public class Image : IEntity
+    {
+        public int ImageId { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
