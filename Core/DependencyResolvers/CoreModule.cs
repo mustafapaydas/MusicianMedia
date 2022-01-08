a2f0b71d3e53d5ee1f75f4058ab6c07cdb5bb739
Mustafa Paydaş <mustafapaydas@hacettepe.edu.tr>
2022-01-08 22:52:27 +0300

    8.01.2022

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Core.CrossCoutingConcerns.Caching;
+using Core.CrossCoutingConcerns.Caching.InMemoryCache;
 using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +14,8 @@ namespace Core.DependencyResolvers
         public void Load(IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton<IHttpContextAccessor,HttpContextAccessor>();
+            serviceCollection.AddMemoryCache();
+            serviceCollection.AddSingleton<ICacheManager,InMemoryCacheManager>();
         }
     }
 }
